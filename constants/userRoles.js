826e0a5a4399de4a92f42bb0f5b23b914fd0d1e0
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2026-01-07 13:43:41 +0530

    feat: Added User model and declared Roles schemas

---
+++ constants/userRoles.js
@@ -0,0 +1,8 @@
+export const USER_ROLES = Object.freeze({
+  SUPER_ADMIN: "super_admin",
+  ADMIN: "admin",
+  TEACHER: "teacher",
+  STUDENT: "student"
+});
+
+export const VALID_ROLES = Object.freeze(Object.values(USER_ROLES));
