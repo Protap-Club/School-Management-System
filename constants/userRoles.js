b19940338e17cff989864806cb0aae627b1720f1
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2026-01-07 14:15:22 +0530

    fix: Remove old constants folder from root

--- constants/userRoles.js
+++
@@ -1,8 +0,0 @@
-export const USER_ROLES = Object.freeze({
-  SUPER_ADMIN: "super_admin",
-  ADMIN: "admin",
-  TEACHER: "teacher",
-  STUDENT: "student"
-});
-
-export const VALID_ROLES = Object.freeze(Object.values(USER_ROLES));
