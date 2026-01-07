a713127834c7fc9164cdb6e37e679c9cb09c1631
Kartik Varia <kartikvaria101@gmail.com>
2026-01-07 16:58:28 +0530

    refactor: rename file and var name

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import router from './routes/index.route.js';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 const app = express();
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1", router);
+app.use("/api/v1/user", userRouter);
 
 // Routes
 app.get('/', (req, res) => {
