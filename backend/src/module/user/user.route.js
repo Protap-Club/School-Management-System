1d260f8272d9b0a28a402e46906e05e69f995463
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2026-03-02 14:39:20 +0530

    feat: add user management API routes with role-based access, validation, and file upload capabilities

--- backend/src/module/user/user.route.js
+++ backend/src/module/user/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { getMyProfile, toggleUserStatus, hardDeleteUsers } from "./user.controller.js";
-import { createUser, getUsers, toggleUserStatus, hardDeleteUsers, uploadAvatar } from "./user.controller.js";
+import { getMyProfile } from "./user.controller.js";
+import { createUser, getUsers, toggleUserStatus, uploadAvatar } from "./user.controller.js";
 import { checkRole } from "../../middlewares/role.middleware.js";
 import { USER_ROLES } from "../../constants/userRoles.js";
 import { validate } from "../../middlewares/validation.middleware.js";
@@ -20,6 +20,6 @@ router.get("/:id", checkWebOnly,getUsers);
 router.post("/", checkWebOnly,checkRole([USER_ROLES.ADMIN, USER_ROLES.TEACHER]), validate(createUserSchema), createUser);
 router.patch("/status", checkWebOnly,checkRole([USER_ROLES.ADMIN, USER_ROLES.TEACHER]), validate(userIdsBodySchema), toggleUserStatus);
 router.patch("/avatar", checkWebOnly,avatarUpload.single("avatar"), uploadAvatar);
-router.delete("/", checkWebOnly,checkRole([USER_ROLES.ADMIN]), validate(userIdsBodySchema), hardDeleteUsers);
+router.delete("/", checkWebOnly,checkRole([USER_ROLES.ADMIN]), validate(userIdsBodySchema));
 
 export default router;
